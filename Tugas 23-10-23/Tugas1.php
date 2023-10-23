47790913a6aac10147195328a8a5b927ccdd89ac
MrahinAp <111964792+MrahinAp@users.noreply.github.com>
2023-10-23 15:17:15 +0700

    Add files via upload

---
+++ Tugas 23-10-23/Tugas1.php
@@ -0,0 +1,9 @@
+<?php
+for($i = 1;$i <=6; $i++){
+    if($i % 2 != 0){
+        echo $i.".Hoby aku adalah koding\n";
+    }else{
+        echo $i. ". Aku sangat senang koding\n";
+    }
+}
+?>
---
+++ Tugas 23-10-23/Tugas4.php
@@ -0,0 +1,11 @@
+<?php
+for($i = 2; $i <= 20; $i += 2){
+    echo $i. "- aku jago koding\n";
+}
+
+echo "LOOPING DUA\n";
+
+for($i = 19; $i >= 1; $i -= 2){
+    echo $i. "- aku seorang web developer\n";
+}
+?>
