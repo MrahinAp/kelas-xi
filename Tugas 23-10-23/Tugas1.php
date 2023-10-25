0cb4c6cbcee1a51d4d1e3d1a1096c1bed1763f7c
MrahinAp <111964792+MrahinAp@users.noreply.github.com>
2023-10-25 13:06:03 +0700

    Tugas.php

--- Tugas 23-10-23/Tugas1.php
+++ Tugas 23-10-23/Tugas1.php
@@ -1,9 +1,42 @@
 <?php
-for($i = 1;$i <=6; $i++){
-    if($i % 2 != 0){
-        echo $i.".Hoby aku adalah koding\n";
+
+//Muhamad Rahin Almasssyah Putra
+//XI-RPL 1
+echo "Bisma Putra Adiyana<br>";
+echo "XI-RPL 1<br><br><br>";
+
+echo "Soal no 1 <br><br>";
+for ($t = 1; $t <= 99; $t +=2) {
+    echo "$t ";
+}
+echo "<br><br><br>";
+
+echo "Soal no 2 <br><br>";
+for ($t = 0; $t <= 100; $t +=8) {
+    echo "$t ";
+}
+echo "<br><br><br>";
+
+echo "Soal  no  3 LOOPING 1<br><br>";
+for ($t = 2; $t <= 20; $t +=2) {
+    echo "$t - Aku Jago Coding <br>";
+}
+echo "<br>";
+echo "Soal no 3 LOOPING 2<br><br>";
+for ($t = 19; $t >= 1; $t -=2) {
+    echo "$t - Aku Seorang Web Developer <br>";
+}
+echo "<br><br><br>";
+
+echo "Soal no  4 <br><br>";
+for($i=1; $i<=25; $i++)
+{
+    if($i % 2 == 0)
+    {
+        echo "$i . Aku Sangat Senang Coding<br>";
     }else{
-        echo $i. ". Aku sangat senang koding\n";
+        echo "$i . Hoby Aku Adalah Coding<br>";
     }
 }
+
 ?>
